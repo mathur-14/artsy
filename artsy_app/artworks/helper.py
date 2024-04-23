aa7c70424d31717b54c391add3950d1614a83c46
Yash Mathur <ym2977@nyu.edu>
2024-04-22 23:07:29 -0400

    BD used session requests

---
+++ artsy_app/artworks/helper.py
@@ -0,0 +1,9 @@
+import requests
+
+def setup_session():
+    """Set up a requests session with a browser-like User-Agent."""
+    session = requests.Session()
+    session.headers.update({
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'
+    })
+    return session
--- artsy_app/artworks/views.py
+++ artsy_app/artworks/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from .helper import setup_session
+import subprocess
 import requests
 import json
 
@@ -8,12 +10,22 @@ def get_token(request):
     cs = '7a1bf2224a3b2aafb0cfcb0c141888fb'
     token_url = f'https://api.artsy.net/api/tokens/xapp_token?client_id={cid}&client_secret={cs}'
 
-    # Make a POST request to get the user token
-    response = requests.post(token_url)
-    data = response.json()
-    user_token = data.get('token', None)
-
-    return JsonResponse({'token': user_token})
+    curl_command = ["curl", "-v", "-X", "POST", token_url]
+    
+    try:
+        # Make a POST request to get the user token
+        response = subprocess.run(curl_command, capture_output=True, text=True, check=True)
+        data = json.loads(response.stdout)
+        user_token = data.get('token', None)
+        if user_token:
+            return JsonResponse({'token': user_token})
+        else:
+            print("Token not found in response")
+            return None
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to obtain XAPP Token: {e}")
+        print(e.stderr)  # Display error output
+        return None
 
 def get_artworks(request):
     # Get the user token by calling the get_token function
@@ -23,12 +35,17 @@ def get_artworks(request):
 
     if user_token is None:
         return JsonResponse({'error': 'Failed to obtain user token'})
+    
+    session = setup_session()
+    session.headers.update({'X-XAPP-Token': user_token})
+
     # API endpoint to get artworks
     artworks_url = 'https://api.artsy.net/api/artworks?total_count=1'
 
     # Make a GET request with the user token in the header
-    headers = {'X-XAPP-Token': user_token}
-    response = requests.get(artworks_url, headers=headers)
-    artworks_data = response.json()
-
-    return JsonResponse(artworks_data)
+    response = session.get(artworks_url)
+    if response.status_code == 200:
+        artworks_data = response.json()
+        return JsonResponse(artworks_data)
+    else:
+        return JsonResponse({'error': f'Failed to fetch artworks. Status code: {response.status_code}'})
